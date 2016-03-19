7dccff09e9b40c247a484d8458cf046640bad79f
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-03-19 21:48:55 +0000

    add callback

--- weather.js
+++ weather.js
@@ -1,21 +1,16 @@
 var request = require('request');
 var url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=b1b15e88fa797225412429c1c50c122a";
-var weather = require('./weather.js');
 
-weather(function (currentWeather) {
-    console.log(currentWeather);
-    });
-
-/*
-request({
+module.exports = function(callback) {
+    request({
     url: url, 
     json: true
     }, function (error, response, body) {
         if (error) {
-         console.log('An error has occured');   
+         callback('An error has occured');   
         } else {
          console.log(JSON.stringify(body, null, 4));
-         console.log("It is " + body.main.temp + " degrees in " + body.name);
+         callback("It is " + body.main.temp + " degrees in " + body.name);
         }
     })
-    */
+}
