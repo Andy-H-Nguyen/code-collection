c4dc82a30308005a9f6e4612cfd2f14e7ad1e73e
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-03-20 06:59:48 +0000

    add ip location

---
+++ app.js
@@ -0,0 +1,11 @@
+var weather = require('./weather.js');
+var location = require('./location.js');
+
+weather(function (currentWeather) {
+    console.log(currentWeather);
+});
+    
+location(function (location) {
+    console.log('city: ' + location.city);
+    console.log('log/lat ' + location.loc);
+});
--- location.js
+++ location.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=b1b15e88fa797225412429c1c50c122a";
+var url = 'http://ipinfo.io';
 
 module.exports = function(callback) {
     request({
@@ -10,7 +10,7 @@ module.exports = function(callback) {
          callback('An error has occured');   
         } else {
          //console.log(JSON.stringify(body, null, 4));
-         callback("It is " + body.main.temp + " degrees in " + body.name);
+         callback(body);
         }
     })
 }
