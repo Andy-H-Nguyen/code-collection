526f3d76c3856ae05c4c0f0bf628f1b2b93fca5d
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-03-21 03:35:41 +0000

    finish callbacks

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 var weather = require('./weather.js');
 var location = require('./location.js');
-
-weather(function (currentWeather) {
-    console.log(currentWeather);
-});
+var argv = require('yargs')
+    .command('create', 'Greets the user', function (yargs){
+      yargs.options({
+            location: {
+                demand: false,
+                alias: 'l',
+                description: 'Your location goes here',
+                type: 'string'
+                }
+         }).help('help')
+    }).help('help').argv;
     
-location(function (location) {
-    console.log('city: ' + location.city);
-    console.log('log/lat ' + location.loc);
-});
+var lookupLocation;
+
+if (typeof argv.l === 'string' && argv.l.length > 0) 
+{
+    console.log("Location given");
+    lookupLocation = argv.l;
+    console.log("Location is " + lookupLocation);
+
+    weather(lookupLocation, function (currentWeather) {
+        console.log(currentWeather);
+    });
+} else {
+    console.log("No location given");
+    location(function (location) {
+        if (!location) {
+            console.log("could not find location");
+            return;
+        }
+        lookupLocation = location.city;
+        console.log("Location is " + lookupLocation);
+
+        weather(lookupLocation, function (currentWeather) {
+            console.log(currentWeather);
+        });
+    });
+}
+
+
--- weather.js
+++ weather.js
@@ -1,7 +1,8 @@
 var request = require('request');
-var url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=b1b15e88fa797225412429c1c50c122a";
 
-module.exports = function(callback) {
+module.exports = function(location, callback) {
+    var encodedLocation = encodeURIComponent(location);
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + encodedLocation + "&units=metric&appid=b1b15e88fa797225412429c1c50c122a";
     request({
     url: url, 
     json: true
