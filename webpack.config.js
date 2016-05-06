e0ab44e56915c2c2f2cc8deaf98ecfd7e50fe10e
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-05-06 07:02:13 +0000

    add react-router

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ module.exports = {
   resolve: {
     root: __dirname,
     alias: {
+      Main: 'app/components/main.jsx',
+      Nav: 'app/components/nav.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
